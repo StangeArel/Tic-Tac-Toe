c6fc02ab2e283f4a05b6462ea6f2b4b01f9fa153
Valeriya Stange <valeriya.stange@gmail.com>
2025-01-31 19:30:27 +0100

    aetValue()

--- script.js
+++ script.js
@@ -25,16 +25,6 @@ function generiereTemplateForStartTable() {
 }
 
 function setValue(fieldIndex) {
-/*     if (fields[fieldIndex] == "") {
-        if (player == "X") {
-            fields[fieldIndex] = "X";
-        } else if (player == "O") {
-            fields[fieldIndex] = "O";
-        }
-        togglePlayer();
-        render();
-    } */
-
     if (player == "X" && fields[fieldIndex] == "") {
         fields[fieldIndex] = "X";
         togglePlayer();
